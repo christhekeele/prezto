50556b4ee1eced89bd1680ad2e89804139568a60
Chris Keele <dev@chriskeele.com>
2014-06-11 09:39:42 -0500

    Add custom battery indicator.

---
+++ modules/battery/init.zsh
@@ -0,0 +1,17 @@
+#
+# Provides battery status functions.
+#
+# Authors:
+#   Chris Keele <dev@chriskeele.com>
+#
+
+# Return if requirements are not found.
+# if (( ! $+commands[git] )); then
+#   return 1
+# fi
+
+# Load dependencies.
+pmodload 'helper'
+
+# # Source module files.
+# source "${0:h}/alias.zsh"
